d9be2a50a4bf114abd3b8248265c178dd535b161
tuppers360 <tuppers360@live.co.uk>
2019-11-03 09:20:32 +0000

    create Stripe Button

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ donation }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_mulYoJrIT0rm9q96siCf8uxp00G8VFbxYd";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Donate"
+      name="Gerry Richardson Trust"
+      billingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Donate"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
