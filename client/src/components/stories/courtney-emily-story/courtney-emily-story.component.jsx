2f1532c6b19e89c21b96ad8b6f5aac74dad01e44
tuppers360 <tuppers360@live.co.uk>
2020-02-29 22:25:10 +0000

    add Ghana story

---
+++ client/src/components/stories/courtney-emily-story/courtney-emily-story.component.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Ghana_1 from "./ghana4.png";
+import "./courtney-emily-story.styles.scss";
+
+export const CourtneyEmilyStory = () => (
+  <React.Fragment>
+    <main>
+      <div className="story-header">
+        <h1>Courtney & Emily's</h1>
+        <h1>Story</h1>
+      </div>
+      <div className="story-container">
+        <img src={Ghana_1} alt="Card" />
+        <p>
+          In March 2018, we were given the amazing opportunity within our
+          nursing studies to travel out of areas to experience different nursing
+          cultures. We decided to travel to Ghana with worktheworld to
+          experience how people who don’t have a system such as the NHS can cope
+          with health and social needs for the public. To be able to do this
+          experience we had a grant from university, but had to fund the rest of
+          the 3 week trip for ourselves. Therefore, we decided to do a sponsored
+          bike ride, alongside working extra hours at our part-time job to be
+          able to fund this trip. We received many donations from friends and
+          family, with the Superintendent Gerry Richardson Memorial Youth Trust
+          kindly donating towards our fundraising, which allowed us to get our
+          remaining vaccinations and medications that we needed to stay safe in
+          Ghana.
+        </p>
+        <p>
+          Whilst in Ghana, we stayed in the local town of Takoradi, where we
+          spent 2 weeks in the Emergency Department of their regional hospital,
+          and then the rest of the time visiting local communities experiencing
+          their different ways of offering health care to all age groups. In the
+          emergency department we were able to share our knowledge with the
+          staff and also learn from themselves about specific illness that they
+          see regularly such as malaria. Also, with the majority of cars in
+          Ghana not having working seatbelts, and there being no systems in
+          place on the roads, road traffic accidents were what brought the most
+          patients to the door of the ED.
+        </p>
+        <p>
+          Whilst visiting the local communities, we visited all ages of people
+          whose nearest hospital was many hours away. During this experience we
+          were able to learn how they provide community care to people who were
+          physically unable to attend the local services, and how they monitor
+          them whilst in their own homes. We also spent the morning in the
+          community speaking with mothers about CPR and how it can save lives,
+          before teaching them how to check if a person has gone into cardiac
+          arrest and then how to correctly perform CPR. We spent time in their
+          community care centre testing locals for malaria, doing regular health
+          and blood pressure checks and providing women with contraception.
+        </p>
+        <p>
+          We spent time with new mothers in the maternity unit where we did
+          checks of the new born babies to ensure that they were healthy after
+          birth. Due to HIV being a widespread virus in Ghana, we also spent
+          time with new mothers and mothers-to-be who had HIV, and performed
+          regular checks of their health both during and after pregnancy.
+        </p>
+        <p>
+          Our time in Ghana is something we will always remember, and is
+          something that we continue to take with us now that we are qualified
+          nurses. It is experiences like this that are eye opening, and make us
+          fortunate for the healthcare that we are able to provide in our
+          country. But also makes us see how incredible the nurses and health
+          care professionals are in Ghana that do their ultimate best to save
+          the lives of their patients with the resources and knowledge that they
+          have.
+        </p>
+      </div>
+    </main>
+  </React.Fragment>
+);
