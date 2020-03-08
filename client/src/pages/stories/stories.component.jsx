6eab90efa75bf815f7b56bed7a7cb557909672d1
tuppers360 <tuppers360@live.co.uk>
2020-03-08 21:19:55 +0000

    style and move to new stories page layout

--- client/src/pages/stories/stories.component.jsx
+++ client/src/pages/stories/stories.component.jsx
@@ -1,52 +1,86 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./stories.styles.scss";
+import { axios } from "axios";
 
 export const Stories = () => (
   <React.Fragment>
+    <h1>Stories</h1>
     <div className="main">
-      <h1>Stories</h1>
-
-      <div className="stories-container">
+      {/*<div className="stories-page-container">
         <div className="stories-item">
-          <img src="/images/nepal.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>Hollies Story</h1>
-            <p>
-              <Link to="/stories/hollies-story">
-                <i className="fab fa-readme"></i> Read
-              </Link>
-            </p>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
         </div>
         <div className="stories-item">
-          <img src="/images/stories.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>More Stories to follow</h1>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
         </div>
         <div className="stories-item">
-          <img src="/images/stories.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>More Stories to follow</h1>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
         </div>
         <div className="stories-item">
-          <img src="/images/stories.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>More Stories to follow</h1>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
         </div>
         <div className="stories-item">
-          <img src="/images/stories.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>More Stories to follow</h1>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
         </div>
         <div className="stories-item">
-          <img src="/images/stories.jpg" alt="Flex Card 1" />
-          <div>
-            <h1>More Stories to follow</h1>
-          </div>
+          <img
+            src="https://source.unsplash.com/random/600x400"
+            alt="Flex Card 1"
+          />
+        </div>
+      </div>*/}
+      <div class="feed">
+        <a class="card" href="/stories/hollies-story">
+          <p class="card__category">Nepal Trip</p>
+          <p class="card__title">Hollie's Story</p>
+        </a>
+        <a class="card" href="/stories/courtney-emily-story">
+          <p class="card__category">Ghana</p>
+          <p class="card__title">Courtney & Emily's Story</p>
+        </a>
+        <div class="card">
+          <p class="card__category">Somewhere</p>
+          <p class="card__title">Dave's Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">Orlando</p>
+          <p class="card__title">Chris' Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">The Muic Festival</p>
+          <p class="card__title">Highfurlong's Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">The Rock School</p>
+          <p class="card__title">Jack's Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">The Footballer</p>
+          <p class="card__title">Brodie's Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">The Karate Kid</p>
+          <p class="card__title">Sienna's Story</p>
+        </div>
+        <div class="card">
+          <p class="card__category">The Vets Adventure</p>
+          <p class="card__title">Mia's Story</p>
         </div>
       </div>
     </div>
