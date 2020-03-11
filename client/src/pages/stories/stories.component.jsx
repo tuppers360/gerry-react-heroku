fe7e622f91e0ec705b5c31119c2365b86bd723fc
tuppers360@live.co.uk <tuppers360@live.co.uk>
2020-03-11 14:54:40 +0000

    updated page and styles for brick work

--- client/src/pages/stories/stories.component.jsx
+++ client/src/pages/stories/stories.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image1 from "./../../assets/images/nepal.jpg";
+import image1 from "./../../assets/images/stories/hollies-story/hollie_1.jpg";
 import image2 from "./../../assets/images/ghana4.png";
 import image3 from "./../../assets/images/image-3.jpg";
 import image4 from "./../../assets/images/image-4.jpg";
@@ -9,211 +9,55 @@ import image6 from "./../../assets/images/image-6.jpg";
 import image7 from "./../../assets/images/image-7.jpg";
 import image8 from "./../../assets/images/image-8.jpg";
 import image9 from "./../../assets/images/image-9.jpg";
+import image10 from "./../../assets/images/image-1.jpg";
 import "./stories.styles.scss";
 
 export const Stories = () => (
   <React.Fragment>
-    <h1>Stories</h1>
+    <h1 className="storyheading">Stories</h1>
     <main>
-      {/*<Link className="stories-page-container">
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-        <Link className="stories-item">
-          <img
-            src="https://source.unsplash.com/random/600x400"
-            alt="Flex Card 1"
-          />
-        </div>
-      </div>
-      <div className="feed">
-        <a className="card" href="/stories/hollies-story">
-          <p className="card__category">Nepal Trip</p>
-          <p className="card__title">Hollie's Story</p>
-        </a>
-        <a className="card" href="/stories/courtney-emily-story">
-          <p className="card__category">Ghana</p>
-          <p className="card__title">Courtney & Emily's Story</p>
-        </a>
-        <div className="card">
-          <p className="card__category">Somewhere</p>
-          <p className="card__title">Dave's Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">Orlando</p>
-          <p className="card__title">Chris' Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">The Muic Festival</p>
-          <p className="card__title">Highfurlong's Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">The Rock School</p>
-          <p className="card__title">Jack's Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">The Footballer</p>
-          <p className="card__title">Brodie's Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">The Karate Kid</p>
-          <p className="card__title">Sienna's Story</p>
-        </div>
-        <div className="card">
-          <p className="card__category">The Vets Adventure</p>
-          <p className="card__title">Mia's Story</p>
-        </div>
-      </div>
-      <Link className="storiescontainer">
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-        <article id="3685" className="location-listing">
-          <a className="location-title" href="#">
-            San Francisco
-          </a>
-          <div className="location-image">
-            <a href="#">
-              <img src={image1} alt="san francisco" />
-            </a>
-          </div>
-        </article>
-      </div>*/}
-
       <div className="grid-container">
         <Link to="/stories/hollies-story" className="storybox a">
           <img src={image1} alt="" />
+          {/* <p className="card__category">Nepal Trip</p> */}
+          <p className="card__title">Hollie's Story</p>
         </Link>
         <Link to="/stories/courtney-emily-story" className="storybox b">
           <img src={image2} alt="" />
+          {/* <p className="card__category">Ghana Trip</p> */}
+          <p className="card__title">Courtney & Emily's Story</p>
         </Link>
-        <Link className="storybox c">
+        <Link to="/stories/" className="storybox c">
           <img src={image3} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox d">
+        <Link to="/stories/" className="storybox d">
           <img src={image4} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox e">
+        <Link to="/stories/" className="storybox e">
           <img src={image5} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox f">
+        <Link to="/stories/" className="storybox f">
           <img src={image6} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox g">
+        <Link to="/stories/" className="storybox g">
           <img src={image7} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox h">
+        <Link to="/stories/" className="storybox h">
           <img src={image8} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox i">
-          <img src={image1} alt="" />
+        <Link to="/stories/" className="storybox i">
+          <img src={image9} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
-        <Link className="storybox j">
-          <img src={image1} alt="" />
+        <Link to="/stories/" className="storybox j">
+          <img src={image10} alt="" />
+          <p className="card__title">More to Follow</p>
         </Link>
       </div>
     </main>
